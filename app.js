612652010349539379ecc7dc0cb6b5e8fbb179f3
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-18 01:31:48 +0500

    modules

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var layer = 5;
 var apple = 5;
-var power = Math.pow(layer, 2);
-console.log(power); //25
+var bags = 2;
+var reminder = apple % bags;
+console.log(reminder);
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
-let layer : number = 5
-let apple : number = 5
-let power : number = layer ** 2
+let apple = 5
+let bags  = 2
 
-console.log(power) //25
+let reminder = apple % bags;
+
+console.log(reminder)
