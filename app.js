56ca574bf7fe2469ba72b57c748c7fe451eae82d
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-18 01:20:38 +0500

    Subtraction

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var num1 = 2;
-var num2 = 3;
-var cart = num1 + num2;
-console.log(cart);
+var cart = 5;
+var num3 = 2;
+var total = cart - num3;
+console.log(total);
--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
-let num1: number = 2;
-let num2: number = 3;
+let cart: number = 5;
+let num3: number =2;
 
-let cart: number= num1 + num2;
-
-console.log(cart)
+let total: number = cart - num3
+console.log(total)
