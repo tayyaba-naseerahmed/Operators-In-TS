93c8abd98a34b2553200fbeb20c12043505de4d0
Tayyaba Naseer Ahmed <tayyabanaseerahmed6@gmail.com>
2024-03-18 01:19:50 +0500

    Addition

---
+++ app.js
@@ -0,0 +1,4 @@
+var num1 = 2;
+var num2 = 3;
+var cart = num1 + num2;
+console.log(cart);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let num1: number = 2;
+let num2: number = 3;
+
+let cart: number= num1 + num2;
+
+console.log(cart)
